1847808d181ba88b15ff88e8bfed2d731b4dddee
giorgiospirostopala <gio.topala@gmail.com>
2025-03-03 23:42:33 +0100

    add: backend setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
-import posts from "./data/posts";
+// import React, { useState } from "react";
+// import posts from "../backend/posts";
 
-function App() {
-  const [listaPost, setListaPost] = useState(posts);
+// function App() {
+//   const [listaPost, setListaPost] = useState(posts);
 
-  return (
-    <div>
-      <h1>Post</h1>
-      <ul>
-        {listaPost.map((post) => (
-          <li key={post.id}>
-            <strong>{post.title}</strong>: {post.content}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Post</h1>
+//       <ul>
+//         {listaPost.map((post) => (
+//           <li key={post.id}>
+//             <strong>{post.title}</strong>: {post.content}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;

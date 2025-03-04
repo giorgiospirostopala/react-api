e2551d1a5ae73e9d5b4e836d3a1f562d195457fa
giorgiospirostopala <gio.topala@gmail.com>
2025-03-04 19:53:00 +0100

    add: backend + cors + vite setup

--- express-blog-api-crud/app.js
+++ express-blog-api-crud/app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const cors = require("cors")
+
 const postRouter = require('./routers/postsRouter');
 
 //middlewares
 const notFound = require('./middlewares/notFound');
 const errorsHandler = require('./middlewares/errorsHandler');
+
+app.use(cors(
+    {
+        origin: "http://localhost:5173"
+    }
+))
 
 app.use(express.static('public'));
 
